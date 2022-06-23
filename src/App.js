431fae6763133327f9d5b1ba67d9a350670f2256
alex.villecco <amvillecco@teco.com.ar>
2022-06-23 00:08:58 -0300

    pass an array of movies to MoviesContainer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import MoviesContainer from './components/MoviesContainer';
 
+const listOfMovies = [
+  'Anna', 'El Arqui', 'The First Lady', 'Balthazar', 'Professor T', 'The Offer',
+];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h2>Flow Unit Test Workshop</h2>
-        <MoviesContainer />
+        <MoviesContainer movies={listOfMovies} />
         <h3>vamo' a aprender snapshot testing</h3>
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/MoviesContainer.js
+++ src/components/MoviesContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './MoviesContainer.css';
 
 const MoviesContainer = ({ movies = [] }) => {
   if (movies.length <= 0) {
