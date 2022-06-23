356567d811dc51d15ae36fe1d11fd2748452e409
alex.villecco <amvillecco@teco.com.ar>
2022-06-22 23:51:30 -0300

    clean App.js and edit test file

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/vamo' a aprender snapshot testing/i);
   expect(linkElement).toBeInTheDocument();
 });
